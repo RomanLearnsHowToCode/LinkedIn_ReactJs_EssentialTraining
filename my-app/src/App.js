b5b6b4fae67cfbd7bce0ec367b6e647fa0028e03
Roman <roman.liewehr@hotmail.com>
2022-01-26 15:43:22 +0000

    changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,8 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h2>Portfolio WebSite under construction</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          26/01/2022 - A whole new website based on React with CI/CD functionality is under construction
         </p>
         <a
           className="App-link"
