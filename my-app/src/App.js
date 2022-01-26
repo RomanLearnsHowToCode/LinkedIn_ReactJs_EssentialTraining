cd6baa092267ac726c5529633563682cb54c73ea
Roman <roman.liewehr@hotmail.com>
2022-01-26 16:35:56 +0000

    my logo update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import './animation';
+import logo2 from './RPortrait.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo2} className="App-logo" alt="logo" />
         <h2>Portfolio WebSite under construction</h2>
         <p>
           26/01/2022 - A whole new website based on React with CI/CD functionality is under construction
