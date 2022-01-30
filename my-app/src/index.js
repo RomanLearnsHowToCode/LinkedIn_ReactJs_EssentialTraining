c73f4c6c8a08558a8f937e8fad1689b526258a9e
Roman <roman.liewehr@hotmail.com>
2022-01-30 20:12:17 +0000

    Commit to save changes

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,7 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.js';
-import Anim from './animation.js';
+
+//  Chapter 04
+
+const checkList = ["boots", "tent", "headlamp"];
+console.log(checkList);
+console.log(checkList[1]);
+
+const [mostImportantItem, second] = ["first item", "second most important thing", "third"];
+console.log(mostImportantItem); // will log first item in array
+console.log(second); // will log out second item in array
+
+const [, , light] = ["item won't be shown", "second item wont be shown", "headlamp"];
+console.log(light) // this will log out headlamp (third item in array)
 
 // Conditionally based rendering
 ReactDOM.render(
