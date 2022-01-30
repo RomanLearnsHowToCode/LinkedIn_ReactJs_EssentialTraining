6de9749a32ad072f4c2f1c4b1133197e2dd1a372
Roman <roman.liewehr@hotmail.com>
2022-01-30 20:50:33 +0000

    REDUCER

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,23 @@
 import './App.css';
 import './animation';
-import React, {useState, useEffect} from "react";
+import React, {useState} from "react";
 
 // Chapter 04, Incorporating useReducer
 
 function App() {
 const [checked, setChecked] = useState(false);
-const [textVariable, setTextVariable] = useState(true);
-// onchange is a function which will setChecked if it is not checked
-// according to the value of this variable we are changing text displayed on line 15
-// the checkbox useState variable true or false are not manipulating the checkbox check..
+
+// Abstracted
+function toggle(){
+  setChecked((checked) => !checked);
+}
+
+// sending function as parameter, this is called REDUCER, it take state and return new state
   return(
     <>
     <input type="checkbox"
-     value={checked} onChange={() => setChecked(checked => !checked)}/>
+     value={checked} onChange={toggle}/>
      <p>{checked ? "checked" : "not checked"}</p>
-
-     <input type="checkbox"
-     value={textVariable} onChange={() => setTextVariable(textVariable => !textVariable)}/>
-     <p>{textVariable ? "text var is true" : "text var is false"}</p>
 
     </>
   )
