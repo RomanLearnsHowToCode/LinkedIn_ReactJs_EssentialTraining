e405c9095ed39891ad450a7c71315dad423ffda7
Roman <roman.liewehr@hotmail.com>
2022-01-30 20:27:15 +0000

    Set emotion variable manipulation with useState

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,23 @@
 import './App.css';
 import './animation';
-
-function SecretComponent(){
-  return <h1>Super secret infromation for authorized users only</h1>;
-}
-
-function RegularComponent(){
-  return <h1>Everyone can see this component</h1>;
-}
+import React, {useState} from "react";
 
 // turnery operator if props.authorized is true then do SecretComponent, else RegularComponent
 // desctructing of objects - props on line 14 were changed for {authorized} and turnery operator is now asking straight for authorized not for props.authorized
-function App({authorized}) {
+function App() {
+  const what = useState("happy"); 
+  console.log(what); // use state will return array
+
+  const [emotion, setEmotion] = useState("in love"); // use state will return pair, state and function, we can setEmotion
+  console.log(emotion);
+
+  // It looks like I could manipulate the state of what is shown (app could return container according to the variable.. basically switching content)
   return(
     <>
-    {authorized ? <SecretComponent /> : <RegularComponent /> }
+    <h1>Current emotion is {emotion}.</h1>
+    <button onClick={() => setEmotion("happy")}>Happy</button>
+    <button onClick={() => setEmotion("frustrated")}>Frustrate</button>
+    <button onClick={() => setEmotion("enthusiastic")}>Enthuse</button>
     </>
   )
 }
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,20 +4,15 @@ import './index.css';
 import App from './App.js';
 
 //  Chapter 04
+// useState
 
 const checkList = ["boots", "tent", "headlamp"];
-console.log(checkList);
-console.log(checkList[1]);
-
 const [mostImportantItem, second] = ["first item", "second most important thing", "third"];
-console.log(mostImportantItem); // will log first item in array
-console.log(second); // will log out second item in array
-
 const [, , light] = ["item won't be shown", "second item wont be shown", "headlamp"];
-console.log(light) // this will log out headlamp (third item in array)
+
 
 // Conditionally based rendering
 ReactDOM.render(
-  <App authorized={false}/>,
+  <App />,
   document.getElementById("root")
   );
