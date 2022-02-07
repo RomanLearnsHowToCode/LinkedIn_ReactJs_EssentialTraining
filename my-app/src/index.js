67c3b1af33635f64ee3dafc856f9f8bee2015545
Roman <roman.liewehr@hotmail.com>
2022-02-07 18:00:01 +0000

    setting up the react router

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App.js';
 
 // Chapter07 Initial commit
+/* React router is a single page website.. basically JavaScript will handle the loading and unloading of content */
+
+
 
 // Setting the login value
 ReactDOM.render(
---
+++ my-app/src/pages.js
@@ -0,0 +1,35 @@
+import React from "react"
+
+// file responsible for all the pages in the app (components)
+
+export function Home(){
+    return(
+        <div>
+            <h1>[Company Wesbite]</h1>
+        </div>
+    )
+}
+
+export function About(){
+    return(
+        <div>
+            <h1>[Company Wesbite]</h1>
+        </div>
+    )
+}
+
+export function Events(){
+    return(
+        <div>
+            <h1>[Company Wesbite]</h1>
+        </div>
+    )
+}
+
+export function Contact(){
+    return(
+        <div>
+            <h1>[Company Wesbite]</h1>
+        </div>
+    )
+}
