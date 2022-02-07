e32c561a2c1bbe03c9eedc4a78d606c06717908a
Roman <roman.liewehr@hotmail.com>
2022-02-07 16:03:32 +0000

    Index file updated

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.js';
 
-// Chapter05 Initial commit
+// Chapter06 Initial commit
 // Setting the login value
 ReactDOM.render(
   <App login="RomanLearnsHowToCode"/>,
